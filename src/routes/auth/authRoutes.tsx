457d0a4ee3f7111c3261ac94871d4e81d88a2d03
AbhayPc <abhaypc112@gmail.com>
2025-02-12 09:56:27 +0530

    Deploy 1

--- src/routes/auth/authRoutes.tsx
+++ src/routes/auth/authRoutes.tsx
@@ -1,10 +1,10 @@
 import Login from '../../pages/auth/Login';
 import Signup from '../../pages/auth/Signup';
 import UserFreelancerOption from '../../pages/auth/UserFreelancerOption';
+import { RouteObject } from "react-router-dom";
 
-// Autherization routes
-export const AuthRoutes = [
-    {path:"/login", element:<Login/>},
-    {path:"/:option/signup", element:<Signup/>},
-    {path:"/option", element:<UserFreelancerOption/>}
+export const AuthRoutes: RouteObject[] = [
+    { path: "/login", element: <Login /> },
+    { path: "/:option/signup", element: <Signup /> },
+    { path: "/option", element: <UserFreelancerOption /> }
 ];
